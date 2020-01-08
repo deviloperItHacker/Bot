5acb04c1bf2991e800911ab12121fdb4794ebc5e
deviloperItHacker <56299838+deviloperItHacker@users.noreply.github.com>
2020-01-08 19:24:31 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,60 @@
+import discord
+from discord import utils
+
+t = 'NjYzODA0NjQ5NTQ3MjM1MzM4.XhOIPQ.sbk-ho3m13P7jfxSTacBeOgx_j8'
+
+POST_ID = 000000000000000000  # Заменить на id будушего поста
+
+DATA = {
+	"1️⃣":658332820841955328, 
+	"2️⃣":658332921823756289,
+	"3️⃣":664005495341711388,
+	"4️⃣":664005500324544513,
+	"5️⃣":658477685944877073,
+	"6️⃣":659489792156762134,
+	"7️⃣":658333153576091662,
+	"8️⃣":659490641885265955,
+	"9️⃣":664005687424188438,
+	"🔟":664005849500352532 ,
+	"🇦":664005801199009822 ,
+	"🇧":660770081789313024 ,
+	"🇨":660770021055660036 ,
+	"🇩":664005894471942187 ,
+}
+
+EXCROLES = [658331716250763284, 658463445783674893, 659854388671545374, 658635192306630658, 658334122028040223] 
+
+class Tupo_Flex(discord.Client):
+	async def on_raw_reaction_add(self, payload):
+		message = await utils.get(utils.get(self.guilds, id=payload.guild_id).text_channels, id=payload.channel_id ).fetch_message(payload.message_id)
+		mem = utils.get(message.guild.members, id=payload.user_id)
+
+		if message.id == POST_ID:
+			s = payload.emoji.name
+			r = utils.get(message.guild.roles, id=DATA[s])
+			if r != None:
+				await mem.add_roles( r )
+				print("[Add Role] {0.name} for {1.display_name}".format(r, mem))
+			else:
+				print("[Error] Role {.name} hasn't been added".format(r))	
+
+	async def on_raw_reaction_remove(self, payload):
+		message = await utils.get(utils.get(self.guilds, id=payload.guild_id).text_channels, id=payload.channel_id ).fetch_message(payload.message_id)
+		mem = utils.get(message.guild.members, id=payload.user_id)
+
+		if message.id == POST_ID:
+			s = payload.emoji.name
+			r = utils.get(message.guild.roles, id=DATA[s])
+			if r != None:
+				await mem.remove_roles(r)
+				print("[Rem Role] {0.name} for {1.display_name}".format(r, mem))
+			else:
+				print("[Error Role] can't remove role {0.name} for {1.display_name}".format(r, mem))		
+
+
+	async def on_ready(self):
+		print("[Log] READY")	
+
+if __name__ == '__main__':
+	c = Tupo_Flex()
+	c.run(t)
